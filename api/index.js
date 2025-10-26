680a41700f2843892f6e050f5198ad7363646ad5
khawar-mahmood <khawermehmood4@gmail.com>
2025-10-26 14:02:54 +0500

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const MONGO_URI = process.env.MONGO_URI;
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectToDatabase() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGO_URI).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default async function handler(req, res) {
+  await connectToDatabase();
+  res.status(200).json({ message: "Node backend running on Vercel!" });
+}
